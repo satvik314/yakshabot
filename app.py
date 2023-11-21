7719a33bc4bac7266674377e6d0d461dfd9f098a
Satvik <satvik.pss@gmail.com>
2023-11-21 11:19:49 +0530

    aider: Initialize ConversationChain and ConversationBufferMemory, and update the chat input to use these classes.
    # Aider chat conversation:
    
    ASSISTANT: To implement this change, we need to modify `app.py` to:
    
    1. Import the necessary classes from Langchain.
    2. Initialize ConversationChain and ConversationBufferMemory.
    3. Update the chat input to use these classes.
    
    Here are the *SEARCH/REPLACE* blocks:
    
    app.py
    ```python
    <<<<<<< SEARCH
    import os
    import openai
    from openai import OpenAI
    import streamlit as st
    from streamlit_message import message
    =======
    import os
    import openai
    from openai import OpenAI
    import streamlit as st
    from streamlit_message import message
    from langchain import ConversationChain, ConversationBufferMemory
    >>>>>>> REPLACE
    ```
    
    app.py
    ```python
    <<<<<<< SEARCH
    os.environ['OPENAI_API_KEY'] = st.secrets.OPENAI_API_KEY
    
    st.title("Yaksha Bot")
    st.subheader("Get interviewed by AI!")
    
    st.chat_input("Enter your query")
    =======
    os.environ['OPENAI_API_KEY'] = st.secrets.OPENAI_API_KEY
    
    st.title("Yaksha Bot")
    st.subheader("Get interviewed by AI!")
    
    # Initialize ConversationChain and ConversationBufferMemory
    conversation_chain = ConversationChain()
    conversation_buffer = ConversationBufferMemory()
    
    # Get user input
    user_input = st.chat_input("Enter your query")
    
    # Pass user input into ConversationChain
    conversation_chain.add_message(user_input)
    >>>>>>> REPLACE
    ```
    
    Please note that this is a basic implementation and may need to be adjusted based on the specific requirements of your application. For example, you may want to handle the case where the user input is empty, or you may want to use the ConversationBufferMemory in some way.

--- app.py
+++ app.py
@@ -3,10 +3,19 @@ import openai
 from openai import OpenAI
 import streamlit as st
 from streamlit_message import message
+from langchain import ConversationChain, ConversationBufferMemory
 
 os.environ['OPENAI_API_KEY'] = st.secrets.OPENAI_API_KEY
 
 st.title("Yaksha Bot")
 st.subheader("Get interviewed by AI!")
 
-st.chat_input("Enter your query")
+# Initialize ConversationChain and ConversationBufferMemory
+conversation_chain = ConversationChain()
+conversation_buffer = ConversationBufferMemory()
+
+# Get user input
+user_input = st.chat_input("Enter your query")
+
+# Pass user input into ConversationChain
+conversation_chain.add_message(user_input)
